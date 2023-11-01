dc439f56651e76db7daa1d769a4dd1963ccbc18e
Thomas <thomas.blakely@gmx.com>
2023-11-02 00:26:21 +1300

    init commit

---
+++ bot.js
@@ -0,0 +1,58 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, client => {
+    console.log("Logged in as:", client.user.tag, "!");
+});
+
+client.commands = new Collection();
+
+const directoryPath = path.join(__dirname, 'commands');
+const commandsDirectories = fs.readdirSync(directoryPath)
+        .filter(directory => !directory.startsWith('.'));
+
+for (const directory of commandsDirectories) {
+  const commandsPath = path.join(directoryPath, directory);
+  const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && !file.startsWith('.'));
+  console.log("Registered: ", commandsFiles);
+  
+  for (const file of commandsFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+  
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log("[WARNING] The command at ", filePath, " is missing a required \"data\" or \"execute\" property.");
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error("No command matching ", Interaction.commandName, " was found. ");
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+    } else {
+      await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+    }
+  }
+
+});
+
+client.login(token);
